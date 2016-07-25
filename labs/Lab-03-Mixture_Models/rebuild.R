9d41dde9bb14fede7878a1c1a630e2df0038c218
James Eddy <james.a.eddy@gmail.com>
2016-07-25 16:53:37 -0700

    starting lab 3

---
+++ labs/Lab-03-Mixture_Models/rebuild.R
@@ -0,0 +1,7 @@
+# rebuild all
+# rmarkdown::render_site()
+# rebuild individual
+rmarkdown::render_site("index.Rmd")
+rmarkdown::render_site("Lab-03-mixture-models-Questions.Rmd")
+rmarkdown::render_site("Lab-03-Poisson-shot-noise.Rmd")
+rmarkdown::render_site("Lab-03-mixture-models-Answers.Rmd")
